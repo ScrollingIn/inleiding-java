36bf326a7cfbabe2608cdded4dde021cacf793e6
ScrollingIn <mdejonge@roc-dev.com>
2018-12-05 12:15:56 +0100

    finished h12

--- src/h12/H12_6.java
+++ src/h12/H12_6.java
@@ -2,18 +2,47 @@ package h12;
 
 import java.awt.*;
 import java.applet.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.*;
 
 public class H12_6 extends Applet {
-    String[] names = { "Valerie", "Jeroen", "Sander", "Frank"};
+    int[] numbers = { 1,6,4,3,3,9,7,3,4,4,7,3,2,5,6,7,8,5,4,4,3 };
+    TextField textfield;
+    int output,totalfound;
+    boolean found;
 
     public void init() {
-        Arrays.sort(names);
+        Arrays.sort(numbers);
+        textfield = new TextField("",2);
+        textfield.addActionListener(new TextFieldListener());
+        found = false;
+        add (textfield);
     }
 
     public void paint(Graphics g) {
-        for (int counter = 0; counter < names.length; counter ++) {
-            g.drawString("" + names[counter], 50, 20 * counter + 20);
+        if (found == true) {
+            g.drawString("is found " + totalfound + " times",230,20);
+        }
+        else {
+            g.drawString("yermom",230,20);
+        }
+    }
+    class TextFieldListener implements ActionListener {
+        public void actionPerformed (ActionEvent e) {
+            totalfound = 0;
+            found = false;
+
+            output = Integer.parseInt(textfield.getText());
+            int counter = 0;
+            while (counter < numbers.length) {
+                if (numbers[counter] == output) {
+                    found = true;
+                    totalfound ++;
+                }
+                counter ++;
+            }
+            repaint();
         }
     }
 }
